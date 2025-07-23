00300c353b7dd59425bcdb0e6ede9f2edf11d41f
sadish <sadish10.sg@gmail.com>
2025-07-23 22:19:27 +0545

    Add project files.

---
+++ Classpractice/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classpractice
+{
+    internal class Car
+    {
+        private Radio radio=new Radio();
+        public void turnswitch(bool turn)
+        {
+            radio.Radioonoff(turn);
+        }
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.turnswitch(false);
+        }
+    }
+}
---
+++ Classpractice/Radio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classpractice
+{
+    internal class Radio
+    {
+        public void Radioonoff(bool switches)
+        {
+            Console.WriteLine("Radio is :{0}", switches);
+        }
+    }
+}
